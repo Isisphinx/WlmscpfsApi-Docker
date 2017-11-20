cf21e18742cfcd7625b9861d730935f0b806c518
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2017-11-20 05:00:39 +0100

    rewrite error in create worklist

--- src/modules/worklist/worklist.js
+++ src/modules/worklist/worklist.js
@@ -4,35 +4,33 @@ const { isNotMemberOfRedisSet, redisAddValueToRedisSet } = require('helpers/redi
 const { pinoPromise } = require('helpers/promise')
 const { pino, worklistDir, worklistListSet } = require('config/constants')
 
-/*
-Create worklist :
-worklist name to lowercase -> check if worklist exist -> Create worklist in db -> create worklist folder -> initialize worklist with lockfile
-*/
 module.exports.createWorklist = (req, res) => {
+  /*
+  worklist name to lowercase -> check if worklist exist -> Create worklist in db -> create worklist folder -> initialize worklist with lockfile
+  */
   const worklistNameLowerCase = stringToLowerCase(req.params.WorklistName)
   const worklistPath = joinPath(worklistDir, worklistNameLowerCase)
   const lockFile = joinPath(worklistPath, 'lockfile')
 
   isNotMemberOfRedisSet(worklistListSet, worklistNameLowerCase, redisClient)
-    .then(data => pinoPromise.trace(data, 'isNotMemberOfRedisSet'))
+    .then(data => pinoPromise.trace(data, 'createWorklist()', 'isNotMemberOfRedisSet'))
 
     .then(([set, value]) => redisAddValueToRedisSet(set, value, redisClient))
-    .then(data => pinoPromise.trace(data, 'redisAddValueToRedisSet'))
+    .then(data => pinoPromise.trace(data, 'createWorklist()', 'redisAddValueToRedisSet'))
 
     .then(([set, value]) => makeDir(worklistPath))
-    .then(data => pinoPromise.trace(data, 'makeDir'))
+    .then(data => pinoPromise.trace(data, 'createWorklist()', 'makeDir'))
 
     .then((path) => writeFile(lockFile, ''))
-    .then(data => pinoPromise.trace(data, 'writeFile'))
+    .then(data => pinoPromise.trace(data, 'createWorklist()', 'writeFile'))
 
     .then(pathLockFile => {
-      pino.debug('Created worklist', worklistNameLowerCase, pathLockFile)
+      pino.debug('createWorklist()', 'Created worklist', worklistNameLowerCase, pathLockFile)
       res.send('200')
       return pathLockFile
     })
     .catch(err => {
-      pino.error('Error trying to create worklist', worklistNameLowerCase, err)
+      pino.error('createWorklist()', 'Error trying to create worklist', worklistNameLowerCase, err)
       res.send('400')
     })
-
 }
