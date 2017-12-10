1a50240175e712ede15b896dc4991efa8dd82933
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2017-12-10 23:21:38 +0100

    chage docker to run server

--- src/app.js
+++ src/app.js
@@ -11,11 +11,9 @@ const { pino } = rootRequire('config/constants')
 /*
 TO DO
 - ioredis using promised version
-- Start listening when db connected stop if db disconnected
 - Refactor purge worklist
 - Gracefully shutdown application on exit signal
 - Change promise to bluebird
-
 - Route function make a function with parameters and switch of returned value for res
 */
 
--- src/server.js
+++ src/server.js
@@ -9,6 +9,11 @@ global.rootRequire = name => require(path.join(__dirname, name))
 const { pino } = rootRequire('config/constants')
 const app = require('./app')
 
+/*
+TO DO
+- Start listening when db connected stop if db disconnected
+*/
+
 app.listen(8080, '0.0.0.0', () => {
   pino.info('App listening on port 3000')
 })
